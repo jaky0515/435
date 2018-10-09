1f4ac934d84d7c698b9c2edcbeb5c26ffd37995b
Kevin Jang <jaky0515@gmail.com>
2018-10-09 16:36:50 -0400

    updates

--- hw2/pt3/MyRSAVerify.py
+++ hw2/pt3/MyRSAVerify.py
@@ -1,28 +1,12 @@
-import json, argparse, hashlib
-from pprint import pprint
+import json, argparse, hashlib, sys
 
 def load_json(file_name):
 	with open(file_name, 'r') as file:
 		data = file.read()
 	return json.loads(data)
 
-def gcd(n, m):
-	if n == 0:
-		return m
-	return gcd(m%n, n)
-
-def euler_totient(n):
-	result = 1
-	for i in range(2, n):
-		if gcd(i, n) == 1:
-			result += 1
-		return result
-
 def create_sign(d, n, M):
 	return pow(M, d, n)
-
-def calc_priv_exp(e, n):
-	return pow(e, -1, euler_totient(n))
 
 """
 main function
@@ -35,10 +19,12 @@ def main():
 	pubkey_data = load_json(pubkey_file_name)
 	sign_data = load_json(sign_file_name)
 
-	d = calc_priv_exp(pubkey_data['e'], pubkey_data['n'])
-	sign = create_sign(d, pubkey_data['n'], int(hashlib.sha256(sign_data['m']).hexdigest(),16))
-
-	pprint(sign)
+	decrypted_sign = create_sign(pubkey_data['e'], pubkey_data['n'], sign_data['sig'])
+	hashed_m = int(hashlib.sha256(sign_data['m']).hexdigest(),16)
+	if decrypted_sign == hashed_m:
+		sys.stdout.write("True\n")
+	else:
+		sys.stdout.write("False\n")
 
 if __name__ == "__main__":
 	main()
