755c310735793589ef4e8b663415849c43212a22
Kevin Jang <jaky0515@gmail.com>
2018-11-27 23:55:29 -0500

    updates

---
+++ hw4/myfingerprinter.py
@@ -0,0 +1,103 @@
+from scapy.all import *
+from pprint import pprint
+import sys
+
+def read_pcap_file( file_name ):
+	'''
+	https://medium.com/@vworri/extracting-the-payload-from-a-pcap-file-using-python-d938d7622d71
+	'''
+	sessions = rdpcap( file_name ).sessions()
+	num_in_packets = 0
+	num_out_packets = 0
+	first_packet_timestamp = None
+	last_packet_timestamp = None
+	for session in sessions:
+		for packet in sessions[session]:
+			if not first_packet_timestamp:
+				first_packet_timestamp = packet[ TCP ].time
+			last_packet_timestamp = packet[ TCP ].time
+
+			if IP in packet:
+				ip_src = packet[ IP ].src
+				ip_dst = packet[ IP ].dst
+			if TCP in packet:
+				tcp_src_port = packet[ TCP ].sport
+				tcp_src_dport = packet[ TCP ].dport
+
+			if packet[ Ether ].src.lower() == get_if_hwaddr( conf.iface ).lower():
+				num_in_packets += 1
+			else:
+				num_out_packets += 1
+	return num_in_packets, num_out_packets, first_packet_timestamp, last_packet_timestamp
+			
+def train():
+	'''
+		
+	'''
+	print( '** Start training...' )
+	pcap_file_names = [
+		'canvas',
+		'google',
+		'tor',
+		'spider',
+		'neverssl',
+		'acm',
+		'autolab',
+	]
+	MAX_ITER = 10
+	model_info = {}
+	for file_name in pcap_file_names:
+		total_num_in_packets = 0
+		total_num_out_packets = 0
+		total_duration = 0
+		for i in range( 0, MAX_ITER ):
+			print( '\t* Reading {}_{}.pcap...'.format( file_name, i ) )
+			num_in_packets, num_out_packets, first_packet_timestamp, last_packet_timestamp = read_pcap_file( '{}_{}.pcap'.format( file_name, i ) )
+			total_num_in_packets += num_in_packets
+			total_num_out_packets += num_out_packets
+			total_duration += last_packet_timestamp - first_packet_timestamp
+		model_info[ file_name ] = {}
+		model_info[ file_name ][ 'num_in_packets' ] = total_num_in_packets / float( MAX_ITER )
+		model_info[ file_name ][ 'num_out_packets' ] = total_num_out_packets / float( MAX_ITER )
+		model_info[ file_name ][ 'duration' ] = total_duration / float( MAX_ITER )
+	return model_info
+
+def test( file_name, model_info ):
+	print( '** Start testing...' )
+	print( '\t* Reading {}...'.format( file_name ) )
+	num_in_packets, num_out_packets, first_packet_timestamp, last_packet_timestamp = read_pcap_file( file_name )
+
+	test_result = {}
+	for site in model_info:
+		# calculate distances
+		distances = [ abs( num_in_packets - model_info[ site ][ 'num_in_packets' ] ), 
+						abs( num_out_packets - model_info[ site ][ 'num_out_packets' ] ),
+						abs( ( last_packet_timestamp - first_packet_timestamp ) - model_info[ site ][ 'duration' ] ) ]
+		# normalize distances
+		norm_distances = [ float( i ) / sum( distances ) for i in distances ]
+		# calculate the average distance
+		test_result[ site ] = ( norm_distances[ 0 ] + norm_distances[ 1 ] + norm_distances[ 2 ] ) / float( len( norm_distances ) )
+
+	matching_site = None
+	matching_site_val = None
+	for site in test_result:
+		if not matching_site or matching_site_val > test_result[ site ]:
+			matching_site = site
+			matching_site_val = test_result[ site ]
+	return matching_site
+
+def main():
+	# validation
+	if len( sys.argv ) != 2:
+		print( "Error: test file name is not provided! Terminating...\n" )
+		exit()
+
+	# train with the training data set
+	model_info = train()
+	# test with the testing data set
+	matching_site = test( sys.argv[ 1 ], model_info )
+	# print result
+	print( '** Result: \"{}\"'.format( matching_site ) )
+
+if __name__ == "__main__":
+	main()
